eede319ddbff9ddfd68faafc2c20d2cb8be7d104
Surjit Kumar Sahoo <ssahoo@msystechnologies.com>
2019-10-23 14:02:55 +0530

    fixed getaddr issue

--- server.c
+++ server.c
@@ -13,69 +13,86 @@ void error(char* msg){
 }
 
 int main(){
+  int sockfd, result,newsockfd, n;
+  char buffer[255];
+  struct addrinfo servaddr, *servinfo, *dup;
+  struct sockaddr_storage connector_addr;
 
+  memset(&servaddr, 0, sizeof servaddr);
+  servaddr.ai_family = AF_INET;
+  servaddr.ai_socktype = SOCK_STREAM;
+  servaddr.ai_flags = AI_PASSIVE;
 
-int sockfd, result,newsockfd, n;
-char buffer[255];
-struct addrinfo servaddr, *servinfo, *dup;
-struct sockaddr_storage connector_addr;
+  result = getaddrinfo(NULL, "3400", &servaddr, &servinfo);
+  if (result != 0) {
+    error("error in getaddrinfo");
+  }
+  
+  /*
+  * getaddrinfo returns a list of addresses, loop through it
+  * Once you find a valid address, create socket
+  * if no address available no need to create socket
+  */
 
-servaddr.ai_family = AF_INET;
-servaddr.ai_socktype = SOCK_STREAM;
-servaddr.ai_flags = AI_PASSIVE;
+  for (dup = servinfo; dup != NULL; dup = dup->ai_next) {
+    /* 
+    * always avaoid duplication, don't use config MACROS everywhere. e.g. in socket()
+    * it's already defined in servaddr struct, use that
+    * Returned real addressess will also have that same config, u can use that too
+    */
+    sockfd = socket(dup->ai_family, dup->ai_socktype, dup->ai_protocol);
+    if (sockfd < 0) {
+      error("unable to create socket");
+    }
 
-result = getaddrinfo(NULL, PORT, &servaddr, &servinfo);
-if (result == 0){
-	//error("errrorrr .......");
-	printf("success");
-	return 1;
-}else{
-	printf("error");
-	return -1;
-}
-//printf("%d: ", result);
- sockfd = socket(AF_INET, SOCK_STREAM, 0);
- if(sockfd < 0){
-	 error("unable to create socket");
-   }
+    if (bind(sockfd, dup->ai_addr, dup->ai_addrlen) == -1) {
+      error("unable to bind the socket");
+    }
+    // once socket binding is successfull, break the loop
+    break;
+  }
+  
+  // free servinfo, because we created the socket successfully
+  freeaddrinfo(servinfo);
+  if (dup == NULL) {
+    error("Failed to bind");
+  }
+
+  if (listen(sockfd, 5) < 0) {
+    error("unable to listen");
+  }
+  
+  printf("listening for connections...\n");
+
+  int conn_addr_len = sizeof(connector_addr);
    
- if(bind(sockfd, dup->ai_addr, dup->ai_addrlen) == -1){
-	 error("unable to bind the socket");
-   }
-   if(listen(sockfd, 5) < 0){
-	   error("unable to listen");
-   }
-   
-   int conn_addr_len = sizeof(connector_addr);
-   
-   newsockfd = accept(sockfd, (struct sockaddr*)&connector_addr, &conn_addr_len);
-   if(newsockfd <0 ){
-	   error("unable to accept");
-   }
+  newsockfd = accept(sockfd, (struct sockaddr*)&connector_addr, &conn_addr_len);
+  if(newsockfd <0 ){
+    error("unable to accept");
+  }
    
   while(1){
-	        memset(buffer, 0, 255);
-            n = read(newsockfd, buffer, 255);
-            if(n < 0){
-                 error("error on receiving");
-            }
-            printf(" client msg : %s\n",buffer);
+    memset(buffer, 0, 255);
+    n = read(newsockfd, buffer, 255);
+    if(n < 0){
+      error("error on receiving");
+    }
+    printf(" client msg : %s\n",buffer);
 			
-            memset(buffer, 0, 255);
-            fgets(buffer, 255, stdin);
+    memset(buffer, 0, 255);
+    fgets(buffer, 255, stdin);
 
-            n = write(newsockfd, buffer, strlen(buffer));
-            if(n<0){
-                error("error on sending");
-            }
-            int i = strncmp("bye", buffer, 3);
-            if(i==0)
-            break;
-        }
+    n = write(newsockfd, buffer, strlen(buffer));
+    if(n<0){
+      error("error on sending");
+    }
+    int i = strncmp("bye", buffer, 3);
+    if(i==0)
+      break;
+  }
  
-    close(newsockfd);
-	close(sockfd);
-	
-	return 0;
+  close(newsockfd);
+  close(sockfd);
 
+  return 0;
 }
